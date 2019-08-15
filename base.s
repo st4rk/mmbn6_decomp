48e321b0ab02f9983fd4cf800c26ea472e429bbd
st4rk <st4rkdev@gmail.com>
2019-08-14 21:30:45 -0700

    removing unused files again

--- base.s
+++ base.s
@@ -1,28 +1,30 @@
 .gba
 
+.relativeinclude on
 .open "base.gba", "output_base.gba", 0x08000000
-
 
 ; it will initialize the gba hardware enveriorment
 ; and jump into the main engine functino
 crt0:
-	.include "crt0.s"
+	.include "src/engine/crt0.s"
 
 ; capcom logo state, its the first state to run
 ; after to initialize the game engine
 
 capcom_logo:
 	; .text implementation
-	.include "capcom_logo.s"
+	.include "src/game/capcom_logo.s"
 	; .data used by capcom logo state
-	.include "capcom_logo_gfx.s"
+	.include "src/game/capcom_logo_gfx.s"
 
+start_screen:
+	.include "src/game/start_screen.s"
 
 ; only uncomment that line if you want to build
 ; custom code hooks into the game engine
 custom_code:
 	; custom code for rom hacking
-	;.include "custom.s"
-
+	;.include "src/custom.s"
+	
 .close
 
